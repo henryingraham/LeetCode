2de8fe3fdb4cc184e437887e0d77ff5ca0041988
henryingraham <92887146+henryingraham@users.noreply.github.com>
2023-08-05 00:59:17 -0400

    Time: 1265 ms (5.00%), Space: 22 MB (36.39%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,18 +5,16 @@ class Solution(object):
         :rtype: List[int]
         """
         product = nums[0]
-        lp = [0, product]
+        lp = [1, product]
         for i in range(1, len(nums)- 1):
             lp.append(nums[i] * product)
             product *= nums[i]
         product = nums[len(nums) - 1]
-        rp = [product, 0]
+        rp = [product, 1]
         for i in range(len(nums) - 2, 0, -1):
             rp.insert(0, nums[i] * product)
             product *= nums[i]
         
-        for i in range(1, len(nums) - 1):
+        for i in range(0, len(nums)):
             nums[i] = lp[i] * rp[i]
-        nums[0] = rp[0]
-        nums[len(nums) - 1] = lp[len(lp) - 1]
         return nums
