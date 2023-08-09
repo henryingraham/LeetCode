0cf3ed361f5a42f77f6e19cb8c6ac2c562331742
henryingraham <92887146+henryingraham@users.noreply.github.com>
2023-08-08 21:41:09 -0400

    Time: 557 ms (84.63%), Space: 25.9 MB (95.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -8,14 +8,12 @@ class Solution(object):
 #         [5, 9, 8, 15, 23]
         if len(nums) == 1:
             return nums[0]
-        dp = {}
-        dp[0] = nums[0]
         maximum = 0
         for i in range(1, len(nums)):
-            dp[i] = max(dp[i-1] + nums[i], nums[i])
-            if dp[i] > dp[maximum]:
+            nums[i] = max(nums[i-1] + nums[i], nums[i])
+            if nums[i] > nums[maximum]:
                 maximum = i
         
-        return dp[maximum]
+        return nums[maximum]
             
         
