50a50c48d58a6f911693401cbff1b78bdcef9291
henryingraham <92887146+henryingraham@users.noreply.github.com>
2023-08-15 02:14:18 -0400

    Time: 47 ms (99.24%), Space: 16.4 MB (36.58%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.py
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -1,20 +1,23 @@
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
-        value = sum(matchsticks)
-        if value < 4:
-            return False
-        if value % 4 != 0:
-            return False
-        edge = value // 4
-        matchsticks.sort(reverse=True)
-        @cache
-        def findedges(l1, l2, l3, l4, i):
-            nonlocal edge
-            if l1 == l2 == l3 == l4 == edge:
+        n, s = len(matchsticks), sum(matchsticks)
+        if s % 4: return False
+        target = s // 4
+        matchsticks.sort(reverse = True)
+        used = [False] * len(matchsticks)
+        def dfs(start, sides, curr):
+            if sides == 4:
                 return True
-            if i > len(matchsticks) - 1:
-                return False
-            if l1 > edge or l2 > edge or l3 > edge or l4 > edge:
-                return False
-            return findedges(l1 + matchsticks[i], l2, l3, l4, i + 1) or findedges(l1, l2 + matchsticks[i] , l3, l4, i + 1) or findedges(l1, l2, l3 + matchsticks[i], l4, i + 1) or findedges(l1, l2, l3, l4 + matchsticks[i] , i + 1)
-        return findedges(0, 0, 0, 0, 0)
+            if curr == target:
+                return dfs(0, sides + 1, 0)
+            for i in range(start, n):
+                if ((i and not used[i - 1] and matchsticks[i] == matchsticks[i - 1]) or 
+                    (used[i] or curr + matchsticks[i] > target)):
+                    continue
+                used[i] = True
+                if dfs(i + 1, sides, curr + matchsticks[i]):
+                    return True
+                used[i] = False
+                if curr == 0:
+                    break
+        return dfs(0, 0, 0)
