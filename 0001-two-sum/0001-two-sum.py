94f315c53999a272f7d078a7a93939a629cec9c6
henryingraham <92887146+henryingraham@users.noreply.github.com>
2023-10-11 15:06:56 -0400

    Time: 62 ms (76.68%), Space: 17.5 MB (43.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        visited = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        s = {}
         for i in range(len(nums)):
-            if target-nums[i] in visited:
-                return [visited[target-nums[i]], i]
+            if target-nums[i] in s.keys():
+                return [i, s[target-nums[i]]]
             else:
-                visited[nums[i]] = i
-            
+                s[nums[i]] = i
