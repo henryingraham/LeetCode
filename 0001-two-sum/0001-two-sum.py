8915c77208d0432d58135e8d53ee6c891ff1d49b
henryingraham <92887146+henryingraham@users.noreply.github.com>
2024-08-04 22:35:09 -0500

    Time: 61 ms (57.82%), Space: 17.8 MB (26.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ans = []
-        mem = {}
-        for i in range(0,len(nums)):
-            if (target-nums[i] in mem.keys()):
-                ans.append(mem[target-nums[i]])
-                ans.append(i)
-                return ans
+        visited = {}
+        for i in range(len(nums)):
+            if target-nums[i] in visited:
+                return [visited[target-nums[i]], i]
             else:
-                mem[nums[i]] = i
+                visited[nums[i]] = i
